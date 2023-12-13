5ef6915bd1d2fc1ea206263543dc5fa36aa11f1e
Krishnakumar Alyanky Dheenabhandu <krishnakumar2004@gmail.com>
2023-12-13 16:27:50 -0500

    module1

---
+++ Module1/AddSubOf2Nums.py
@@ -0,0 +1,9 @@
+# Get 2 inputs of string from user
+# convert i/p string to int
+# Add two int values and print as o/p
+# Sub two int values and print as o/p
+
+num1 = int(input('Enter first Num to Add/Sub '))
+num2 = int(input('Enter second Num to Add/Sub '))
+print('Add of 2 numbers',num1,'+',num2,'=',num1+num2)
+print('Sub of 2 numbers',num1,'-',num2,'=',num1-num2)
---
+++ Module1/MultiDiviOf2Nums.py
@@ -0,0 +1,9 @@
+# Get 2 inputs of string from user
+# convert i/p string to int
+# Multiply two int values and print as o/p
+# Divide two int values and print as o/p
+
+num1 = int(input('Enter first Num to Mul/Div '))
+num2 = int(input('Enter second Num to Mul/Div '))
+print('Multiply of 2 numbers',num1,'*',num2,'=',num1*num2)
+print('Division of 2 numbers',num1,'/',num2,'=',num1/num2)
