335c8a822d3caa9d272fa7080473b102bcb254e6
Krishnakumar Alyanky Dheenabhandu <krishnakumar2004@gmail.com>
2024-01-07 09:46:12 -0500

    used dict
    used dict for total value

--- Module4/OnlineShopCart.py
+++ Module4/OnlineShopCart.py
@@ -1,26 +1,24 @@
 class ItemToPurchase :
-
     def __init__(self):
         item_name = 'none'
         item_price = float(0)
         item_quantity = 0
 
-    def print_item_cost(self,total):
-        print('{} {} @ ${} = ${}'.format(self.item_name, self.item_quantity, self.item_price, total))
+    def print_item_cost(self):
+        print('{} {} @ ${} = ${}'.format(self.item_name, self.item_quantity, self.item_price, self.item_price * self.item_quantity))
 
-itemToPurchase1 = ItemToPurchase()
-itemToPurchase1.item_name = input('Enter the item name:')
-itemToPurchase1.item_price = float(input('Enter the item price:'))
-itemToPurchase1.item_quantity = int(input('Enter the item quantity:'))
+listOfItemToPurchase = []
+dictionaryOfTotal = {"total":0}
 
-itemToPurchase2 = ItemToPurchase()
-itemToPurchase2.item_name = input('Enter the item name:')
-itemToPurchase2.item_price = float(input('Enter the item price:'))
-itemToPurchase2.item_quantity = int(input('Enter the item quantity:'))
+for i in range(2) :
+    itemToPurchase1 = ItemToPurchase()
+    itemToPurchase1.item_name = input('Enter the item name:')
+    itemToPurchase1.item_price = float(input('Enter the item price:'))
+    itemToPurchase1.item_quantity = int(input('Enter the item quantity:'))
+    listOfItemToPurchase.append(itemToPurchase1)
+    dictionaryOfTotal["total"] = dictionaryOfTotal["total"] + (itemToPurchase1.item_price * itemToPurchase1.item_quantity)
 
 print('TOTAL COST')
-total1 = itemToPurchase1.item_price * itemToPurchase1.item_quantity
-total2 = itemToPurchase2.item_price * itemToPurchase2.item_quantity
-itemToPurchase1.print_item_cost(total1)
-itemToPurchase2.print_item_cost(total2)
-print('Total: ${}'.format(total1 + total2))
+for itemToPurchase in listOfItemToPurchase :
+    itemToPurchase.print_item_cost()
+print('Total: ${}'.format(dictionaryOfTotal["total"]))
